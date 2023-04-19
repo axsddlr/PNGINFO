5ea8426cca9368f694a599765a3c9eef872981d6
Andre Saddler <dresadd09691@gmail.com>
2023-04-19 01:58:32 -0400

    refactor(HOF.py): remove on_raw_reaction_remove listener from HofCog class fix(HOF.py): add delay before fetching message to ensure it is cached

--- cogs/HOF.py
+++ cogs/HOF.py
@@ -22,6 +22,8 @@ class HofCog(commands.Cog):
         if payload.channel_id == initial_channel_id:
             channel = self.bot.get_channel(payload.channel_id)
             message = await channel.fetch_message(payload.message_id)
+            await asyncio.sleep(5)
+            message = await channel.fetch_message(payload.message_id)
             star_count = sum([r.count for r in message.reactions if r.emoji == "⭐"])
 
             destination_channel_id = TARGET_CHANNEL_ID
@@ -45,11 +47,6 @@ class HofCog(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        if payload.emoji.name == "⭐":
-            await self.check_star_count(payload)
-
-    @commands.Cog.listener()
-    async def on_raw_reaction_remove(self, payload):
         if payload.emoji.name == "⭐":
             await self.check_star_count(payload)
 
