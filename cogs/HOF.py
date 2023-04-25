2174a62f35ee210da027dcacefac0d27fab5f908
Andre Saddler <dresadd09691@gmail.com>
2023-04-25 13:50:49 -0400

    refactor(HOF.py): remove unused imports and methods, simplify message sending logic

--- cogs/HOF.py
+++ cogs/HOF.py
@@ -1,5 +1,4 @@
 import discord
-from discord import Permissions
 from discord.ext import commands
 import json
 import asyncio
@@ -25,22 +24,6 @@ class HofCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.sent_messages = set()
-        self.target_channel_permissions = None
-
-    async def get_target_channel_permissions(self):
-        guild = self.bot.get_guild(TARGET_GUILD_ID)
-        channel = guild.get_channel(TARGET_CHANNEL_ID)
-        me = guild.me
-        return channel.permissions_for(me)
-
-    def has_required_permissions(self, permissions: Permissions):
-        required_permissions = ["send_messages", "embed_links"]
-        missing_permissions = [
-            perm_name
-            for perm_name in required_permissions
-            if not getattr(permissions, perm_name)
-        ]
-        return len(missing_permissions) == 0, missing_permissions
 
     async def check_unique_reactions(self, payload):
         initial_channel_id = INITIAL_CHANNEL_ID
@@ -67,51 +50,19 @@ class HofCog(commands.Cog):
                     f"Sending message to {destination_channel.name} in {destination_channel.guild.name}"
                 )
 
-                has_perms, missing_perms = self.has_required_permissions(
-                    self.target_channel_permissions
+                content = (
+                    f"**Reactions:** {len(unique_users)} | **{message.channel.name}**\n"
+                    f"**Original Post:** [Original Post](https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id})\n"
+                    f"**Author:** {message.author}\n"
                 )
-                if not has_perms:
-                    logger.warning(
-                        f"Missing permissions in target channel: {', '.join(missing_perms)}"
-                    )
-                    return
-                else:
-                    logger.info("Has permissions")
+                if message.attachments and message.attachments[0].url:
+                    content += f"**Attachment:** {message.attachments[0].url}\n"
 
-                    if self.target_channel_permissions.embed_links:
-                        embed = discord.Embed(
-                            title=f"Reactions: {len(unique_users)} | {message.channel.name}",
-                            description=f"[Original Post](https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id})",
-                            timestamp=message.created_at,
-                            color=discord.Color.gold(),
-                        )
-                        embed.set_author(
-                            name=f"{message.author}",
-                            icon_url=message.author.avatar.url,
-                        )
-                        if message.attachments and message.attachments[0].url:
-                            embed.set_image(url=message.attachments[0].url)
-
-                        await destination_channel.send(embed=embed)
-                    else:
-                        content = (
-                            f"**Reactions:** {len(unique_users)} | **{message.channel.name}**\n"
-                            f"**Original Post:** [Original Post](https://discord.com/channels/{message.guild.id}/{message.channel.id}/{message.id})\n"
-                            f"**Author:** {message.author}\n"
-                        )
-                        if message.attachments and message.attachments[0].url:
-                            content += f"**Attachment:** {message.attachments[0].url}\n"
-
-                        await destination_channel.send(content)
+                await destination_channel.send(content)
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         await self.check_unique_reactions(payload)
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        self.target_channel_permissions = await self.get_target_channel_permissions()
-        logger.info("target_channel_permissions checked")
 
 
 async def setup(bot):
